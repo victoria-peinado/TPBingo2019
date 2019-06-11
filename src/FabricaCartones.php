86ea710c56182102e714b59301e69d458e6c91ce
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2019-06-11 16:03:48 -0300

    añadido FabricaCoarton

---
+++ src/FabricaCartones.php
@@ -0,0 +1,103 @@
+<?php
+namespace Bingo;
+class FabricaCartones {
+  public function generarCarton() {
+    // Algo de pseudo-código para ayudar con la evaluacion.
+    $carton = $this->intentoCarton();
+    if ($this->cartonEsValido($carton)) {
+      return $carton;
+    }
+  }
+  protected function cartonEsValido($carton) {
+    if (validarUnoANoventa($carton) &&
+      validarCincoNumerosPorFila($carton) &&
+      validarColumnaNoVacia($carton) &&
+      validarColumnaCompleta($carton) &&
+      validarTresCeldasIndividuales($carton) &&
+      validarNumerosIncrementales($carton) &&
+      validarFilasConVaciosUniformes($carton)
+    ) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+  protected function validarUnoANoventa($carton) {
+  }
+  protected function validarCincoNumerosPorFila($carton) {
+  }
+  protected function validarColumnaNoVacia($carton) {
+  }
+  protected function validarColumnaCompleta($carton) {
+  }
+  protected function validarTresCeldasIndividuales($carton) {
+  }
+  protected function validarNumerosIncrementales($carton) {
+  }
+  protected function validarFilasConVaciosUniformes($carton) {
+  }
+  public function intentoCarton() {
+    $contador = 0;
+    $carton = [
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0]
+    ];
+    $numerosCarton = 0;
+    while ($numerosCarton < 15) {
+      $contador++;
+      if ($contador == 50) {
+        return $this->intentoCarton();
+      }
+      $numero = rand (1, 90);
+      $columna = floor ($numero / 10);
+      if ($columna == 9) { $columna = 8;}
+      $huecos = 0;
+      for ($i = 0; $i<3; $i++) {
+        if ($carton[$columna][$i] == 0) {
+          $huecos++;
+        }
+        if ($carton[$columna][$i] == $numero) {
+          $huecos = 0;
+          break;
+        }
+      }
+      if ($huecos < 2) {
+        continue;
+      }
+      $fila = 0;
+      for ($j=0; $j<3; $j++) {
+        $huecos = 0;
+        for ($i = 0; $i<9; $i++) {
+          if ($carton[$i][$fila] == 0) { $huecos++; }
+        }
+        if ($huecos < 5 || $carton[$columna][$fila] != 0) {
+          $fila++;
+        } else{
+          break;
+        }
+      }
+      if ($fila == 3) {
+        continue;
+      }
+      $carton[$columna][$fila] = $numero;
+      $numerosCarton++;
+      $contador = 0;
+    }
+    for ( $x = 0; $x < 9; $x++) {
+      $huecos = 0;
+      for ($y =0; $y < 3; $y ++) {
+        if ($carton[$x][$y] == 0) { $huecos++;}
+      }
+      if ($huecos == 3) {
+        return $this->intentoCarton();
+      }
+    }
+    return $carton;
+  }
+}
